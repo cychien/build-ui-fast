cc126823df52b063db76bc836b4c53a7d6834663
cychien <xyz030206@gmail.com>
2023-12-19 20:27:59 +0800

    feat: add grid components

---
+++ app/components/builduifast/grid/FluidGrid/Grid.tsx
@@ -0,0 +1,11 @@
+function Grid() {
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,_minmax(150px,_1fr))] gap-3">
+      <div>Content</div>
+      <div>Content</div>
+      <div>Content</div>
+    </div>
+  );
+}
+
+export { Grid };
