4bdaab656d71dbf2090e980422b7b92f1ce18da3
cychien <xyz030206@gmail.com>
2024-01-01 19:15:00 +0800

    update

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
   ScrollRestoration,
   useLocation,
 } from "@remix-run/react";
-import Header from "~/components/builduifast/header/SlideDownHeader";
+import { Header } from "~/components/site/Header";
 
 export const links: LinksFunction = () => [
   {
