fdcd34658b460f918b37c11622e55c48425eaebe
cychien <xyz030206@gmail.com>
2024-01-28 14:59:57 +0800

    feat: add more heroes

---
+++ app/components/builduifast/hero/HeroRightFullPage.tsx
@@ -0,0 +1,90 @@
+import { Announcement } from "../announcement/Announcement";
+import { Button } from "../button/Button";
+import { MonitorPlay } from "lucide-react";
+
+// TODO: use image component
+
+function Hero() {
+  return (
+    <div className="grid h-dvh grid-rows-[auto,_1fr] gap-y-16 lg:grid-cols-2 lg:grid-rows-none">
+      <div className="px-4 pt-16 md:pl-10 md:pr-10 lg:max-w-[512px] lg:self-center lg:justify-self-end lg:pl-8 lg:pr-20 lg:pt-0 xl:max-w-[640px] 2xl:max-w-[768px]">
+        <Announcement />
+        <div className="mt-8">
+          <h1 className="text-4xl font-semibold leading-[44px] md:max-w-screen-sm md:text-6xl md:leading-[120%] lg:max-w-screen-md xl:max-w-screen-lg">
+            提高生產力的極簡日曆
+          </h1>
+        </div>
+        <div className="mt-6 flex">
+          <p className="text-lg text-gray-600 md:max-w-xl md:text-xl md:leading-[150%] lg:max-w-screen-sm xl:max-w-screen-md">
+            一款結合極簡風格和強大功能的新世代日曆軟體。內置先進 AI
+            技術，讓您的時間管理更加高效，輕鬆完成每一項任務。
+          </p>
+        </div>
+        <div className="mt-8 flex flex-col space-y-3 sm:flex-row sm:space-x-3 sm:space-y-0 md:mt-12">
+          <Button variant="outline" size="xl">
+            <MonitorPlay className="mr-2.5 size-5" />
+            預約 Demo
+          </Button>
+          <Button size="xl">註冊</Button>
+        </div>
+      </div>
+
+      <img
+        src="https://res.cloudinary.com/dgppby8lr/image/upload/v1704124379/build-ui-fast/demo_hjwkqa.avif"
+        alt=""
+        width={2940}
+        height={1960}
+        className="h-full object-cover lg:self-center"
+      />
+    </div>
+  );
+}
+
+function Demo() {
+  return <Hero />;
+}
+
+const templateCode = `\
+  import { Announcement } from "~/components/Announcement";
+  import { Button } from "~/components/Button";
+  import { MonitorPlay } from "lucide-react";
+
+  function Hero() {
+    return (
+      <div className="grid h-dvh grid-rows-[auto,_1fr] gap-y-16 lg:grid-cols-2 lg:grid-rows-none">
+        <div className="px-4 pt-16 md:pl-10 md:pr-10 lg:max-w-[512px] lg:self-center lg:justify-self-end lg:pl-8 lg:pr-20 lg:pt-0 xl:max-w-[640px] 2xl:max-w-[768px]">
+          <Announcement />
+          <div className="mt-8">
+            <h1 className="text-4xl font-semibold leading-[44px] md:max-w-screen-sm md:text-6xl md:leading-[120%] lg:max-w-screen-md xl:max-w-screen-lg">
+              提高生產力的極簡日曆
+            </h1>
+          </div>
+          <div className="mt-6 flex">
+            <p className="text-lg text-gray-600 md:max-w-xl md:text-xl md:leading-[150%] lg:max-w-screen-sm xl:max-w-screen-md">
+              一款結合極簡風格和強大功能的新世代日曆軟體。內置先進 AI
+              技術，讓您的時間管理更加高效，輕鬆完成每一項任務。
+            </p>
+          </div>
+          <div className="mt-8 flex flex-col space-y-3 sm:flex-row sm:space-x-3 sm:space-y-0 md:mt-12">
+            <Button variant="outline" size="xl">
+              <MonitorPlay className="mr-2.5 size-5" />
+              預約 Demo
+            </Button>
+            <Button size="xl">註冊</Button>
+          </div>
+        </div>
+
+        <img
+          src="https://res.cloudinary.com/dgppby8lr/image/upload/v1704124379/build-ui-fast/demo_hjwkqa.avif"
+          alt=""
+          width={2940}
+          height={1960}
+          className="h-full object-cover lg:self-center"
+        />
+      </div>
+    );
+  }
+`;
+
+export default Demo;
+export { templateCode };
--- app/data/component.ts
+++ app/data/component.ts
@@ -1,8 +1,12 @@
 export const componentNames: Record<string, string> = {
   header: "Header",
+  hero: "Hero",
+  footer: "Footer",
   grid: "Grid",
-  button: "Button",
   "multi-columns-layout": "多欄位排版",
-  resizable: "可伸縮元素",
   background: "背景",
+  button: "Button",
+  announcement: "Announcement",
+  tabs: "Tabs",
+  resizable: "可伸縮元素",
 };
