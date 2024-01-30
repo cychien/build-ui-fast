32d50c07496fcc8daa37375e9cad8ba5d27ac05d
cychien <xyz030206@gmail.com>
2024-01-30 23:14:10 +0800

    feat: add entry animation

--- app/routes/components._index/route.tsx
+++ app/routes/components._index/route.tsx
@@ -78,13 +78,13 @@ export default function ComponentsPage() {
           name="各式效果"
           description="玉山山貌高峻，最高峰玉山主峰四面皆是陡壁危崖，南北兩側是千仞峭壁，西側絕壑深"
         >
-          {/* <ComponentsGrid>
+          <ComponentsGrid>
             <ComponentCard
-              name="漸層"
-              componentId="gradient"
+              name="進場動畫"
+              componentId="entry-animation"
               // imageUrl="https://res.cloudinary.com/dgppby8lr/image/upload/v1703677916/build-ui-fast/Header-03_zlko7x.png"
             />
-          </ComponentsGrid> */}
+          </ComponentsGrid>
         </Row>
       </div>
     </div>
